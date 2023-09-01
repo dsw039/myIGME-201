4459be28718e484cba4f1c7aaa6c714481ab5537
dwright60 <124175902+dwright60@users.noreply.github.com>
2023-09-01 16:38:34 -0400

    hello bros

---
+++ Wright_HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wright_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            Console.WriteLine("Daniel Wright");
+            Console.WriteLine("5 plus 3 is " + (num1 + num2));
+        }
+    }
+}
