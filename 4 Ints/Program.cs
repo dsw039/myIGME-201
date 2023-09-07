9f59daa8844f50e1fc171a6f6c8160eaade7f789
dwright60 <124175902+dwright60@users.noreply.github.com>
2023-09-06 21:39:55 -0400

    doneson

---
+++ 4 Ints/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Ints
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int cant be 0 because anything times 0 is 0
+            int product = 1;
+            //tell user to give numbers
+            Console.WriteLine("enter 4 numbers.");
+            //grabs a number and multiplies it with our first number 4 times.
+            for (int i = 0; i < 4; i++)
+            {
+                //grabs the input
+                string r = Console.ReadLine();
+                //turns the string into an int var
+                int tempNum = Convert.ToInt32(r);
+                //mutiplies it
+                product *= tempNum;
+            }
+            //displays final product
+            Console.WriteLine("product is "+product);
+        }
+    }
+}
