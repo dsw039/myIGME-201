79f6312b5f9f636ac3344c2dd25bee37a4a8ab9a
dwright60 <124175902+dwright60@users.noreply.github.com>
2023-09-10 23:40:21 -0400

    gg

--- Var10/Program.cs
+++ Var10/Program.cs
@@ -7,13 +7,15 @@ using System.Threading.Tasks;
 
 namespace Var10
 {
+    //This program takes two numbers(provided by the user) and compares them with 10. If one(but not both) are greater than 10, they are displayed
     internal class Program
     {
+        //if one(but not both) of the user provided numbers are greater than 10, they are both displayed, otherwise they are rejected.
         static void Main(string[] args)
         {
             //two variables to hold the numbers we will compare
-            int var1 = 0;
-            int var2 = 0;
+            int? var1 = null;
+            int? var2 = null;
 
             //tell user to enter a number
             Console.WriteLine("Enter a number");
@@ -23,25 +25,56 @@ namespace Var10
             Console.WriteLine("Enter another number");
             //retrieves second number from user
             String secondNum = Console.ReadLine();
-            //converts the inputs into int
-            var1 = Convert.ToInt32(firstNum);
-            var2 = Convert.ToInt32(secondNum);
-
-            //boolean var
-            Boolean ten = true;
-
-            
-            
-               if ((var1 > 10 || var2 > 10))
+            //tries to convert the input into int while the int is null
+            while (var1 == null)
             {
+                try
+                {
+                    var1 = Convert.ToInt32(firstNum);
+                }
+                catch
+                {
+                    Console.WriteLine("Enter a number");
+                    firstNum = Console.ReadLine();
+                }
+            }
+            
+            while (var2 == null)
+            {
+                try
+                {
+                    var2 = Convert.ToInt32(secondNum);
+                }
+                catch
+                {
+                    Console.WriteLine("Enter a number");
+                     secondNum = Console.ReadLine();
+                }
+            }
+
+            
+
+            
+            //boolean var to check if our inputs work
+            bool ten = false;
+
+            
+            
+               if (var1 > 10 ^ var2 > 10) {
                 ten = true;
             }
-               else if (!(var1 > 10 && var2 > 10) || (var1 > 10 && var2 > 10))
-            {
-                ten = false;
-            }
 
-            Console.WriteLine(ten);
+            //if it works then the numbers are displayed
+            if (ten)
+            {
+                Console.WriteLine(var1);
+                Console.WriteLine(var2);
+            }
+            //if not then they are rejected
+            else
+            {
+                Console.WriteLine("Rejected!");
+            }
 
         }
     }
