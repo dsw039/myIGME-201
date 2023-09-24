864571558038960dd21a31b836e36355af583305
dwright60 <124175902+dwright60@users.noreply.github.com>
2023-09-24 16:54:28 -0400

    i give up

---
+++ StringDelegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDelegate
+{
+    delegate string ConsoleFunction(string input);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ConsoleFunction fakeRead;
+
+            fakeRead = new ConsoleFunction(Read);
+
+            String readLine = fakeRead(input);
+        }
+
+        static string Read(string lineInput) {
+        
+            return Console.ReadLine();
+        }
+    }
+}
