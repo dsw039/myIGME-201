8982b91c8f6fd025f2c2cd3e01d93311a374a296
dwright60 <124175902+dwright60@users.noreply.github.com>
2023-10-01 02:37:55 -0400

    Raise it up!

---
+++ RaiseItUp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaiseItUp
+{
+    internal class Program
+    {
+        //This programs grabs a string from the user and procceeds adds to a double value if the string matches "Daniel"
+        static void Main(string[] args)
+        {
+            //variables
+            string sName;
+            double dSalary = 30000;
+
+            //prompt user for name(string)
+            Console.WriteLine("What is your name?");
+           
+           //gives the sName string the value of the user's string
+                sName = Console.ReadLine();
+            
+           
+            //call to function
+            GiveRaise(sName, ref dSalary);
+        }
+
+        //this function compares the user's string to "Daniel". If it matches, salary is increased by 19999.99.
+        static bool GiveRaise( string name, ref double salary)
+        {
+            if (name == "Daniel")
+            {
+                salary += 19999.99;
+                Console.WriteLine("Congrats! You got a raise!");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
