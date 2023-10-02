406bd1b8a236cd5f30754846ef45e4bbe41c3acb
dwright60 <124175902+dwright60@users.noreply.github.com>
2023-10-01 23:50:28 -0400

    time up

---
+++ EmployeeStruct/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EmployeeStruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //GiveRaise(emp);
+        }
+
+        struct Employee
+        {
+            public string sName;
+            public double dSalary;
+        }
+
+        static  bool GiveRaise(Employee emp)
+        {
+            emp.dSalary = 30000;
+            Console.WriteLine("What is your name?");
+            emp.sName = Console.ReadLine();
+            if (emp.sName == "Daniel")
+            {
+                emp.dSalary += 19999.99;
+                Console.WriteLine("Congrats! You got a raise! Your new Salary is " + emp.dSalary);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ErrorTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[][] dArray = new double[2][];
+            dArray[0] = new double[1];
+            dArray[1] = new double[0];
+
+            dArray[0][0] = 15;
+            dArray[1][0] = 5.67;
+
+        }
+    }
+}
