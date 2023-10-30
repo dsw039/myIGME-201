b26a17b8209b0f08087180355b7d2f36665409d8
dsw039 <d.s.wright2022@gmail.com>
2023-10-29 21:04:14 -0400

    done?

---
+++ UnitTwoNum14/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTwoNum14
+{
+    public class Friend
+    {
+        public string name;
+        public string greeting;
+        public DateTime birthdate;
+        public string address;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Friend friend = new Friend();
+            Friend enemy = new Friend();
+            
+            // change friend to Charlie Sheen
+            friend.name = "Charlie Sheen";
+            friend.greeting = "Dear Charlie";
+            friend.birthdate = DateTime.Parse("1967-12-25");
+            friend.address = "123 Any Street, NY NY 12202";
+
+            
+            enemy = friend;
+
+            enemy.greeting = "Sorry Charlie";
+            enemy.address = "Return to sender.  Address unknown.";
+
+            Console.WriteLine($"friend.greeting => enemy.greeting: {friend.greeting} => {enemy.greeting}");
+            Console.WriteLine($"friend.address => enemy.address: {friend.address} => {enemy.address}");
+        }
+    }
+}
