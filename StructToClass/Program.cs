1eddce5b609994cdf888a2db7fa353c5920df05f
dsw039 <d.s.wright2022@gmail.com>
2023-10-29 21:10:06 -0400

    comment out enemy =

--- StructToClass/Program.cs
+++ StructToClass/Program.cs
@@ -4,31 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UnitTwoNum14
+namespace StructToClass
 {
-    public class Friend
+    struct Friend
     {
         public string name;
         public string greeting;
         public DateTime birthdate;
         public string address;
     }
-    internal class Program
+
+    class Program
     {
+
         static void Main(string[] args)
         {
-            Friend friend = new Friend();
-            Friend enemy = new Friend();
-            
-            // change friend to Charlie Sheen
+            Friend friend;
+            Friend enemy;
+
+            // create my friend Charlie Sheen
             friend.name = "Charlie Sheen";
             friend.greeting = "Dear Charlie";
             friend.birthdate = DateTime.Parse("1967-12-25");
             friend.address = "123 Any Street, NY NY 12202";
 
-            
-           // enemy = friend;
+            // now he has become my enemy
+            enemy = friend;
 
+            // set the enemy greeting and address without changing the friend variable
             enemy.greeting = "Sorry Charlie";
             enemy.address = "Return to sender.  Address unknown.";
 
@@ -36,4 +39,5 @@ namespace UnitTwoNum14
             Console.WriteLine($"friend.address => enemy.address: {friend.address} => {enemy.address}");
         }
     }
+
 }
