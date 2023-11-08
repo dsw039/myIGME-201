0fb620a2018e7a98d819750aa24736b9cd1de70f
dsw039 <d.s.wright2022@gmail.com>
2023-11-08 00:39:18 -0500

    long awaited

---
+++ UnitTwoTester/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTwoTester
+{
+    static class Program
+    {
+        public class MyClass
+        {
+            public int myInt;
+
+            public MyClass(int nVal)
+            {
+                this.myInt = nVal;
+            }
+        }
+
+        public class MyDerivedClass : MyClass
+        {
+            public MyDerivedClass(int nVal) : base(nVal)
+            {
+                this.myInt = (this.myInt + 2) * 4;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MyDerivedClass myObj = new MyDerivedClass(42);
+            Console.WriteLine(myObj.myInt);
+        }
+    
+        
+    }
+}
